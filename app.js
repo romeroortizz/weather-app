0b6c45cdf2542a1dc1f92e821600c79d1c19142e
Romero <175451535+romeroortizz@users.noreply.github.com>
2025-03-05 21:53:17 -0600

    added weather api using async and await

--- app.js
+++ app.js
@@ -6,14 +6,12 @@ if (navigator.geolocation) {
         getCordinates(position)
     })
 } 
-
 function getCordinates(position) {
      positionX = position.coords.latitude
      positionY= position.coords.longitude  
     getCityName(positionX,positionY)
 }
-
-// AIzaSyDDAVn2_WR0QjOgRKS7HnMzz0gdgpQ0fKo
+// Google geolocation API key
 
 const API_KEY = "AIzaSyDDAVn2_WR0QjOgRKS7HnMzz0gdgpQ0fKo"
 function getCityName(lat, long) {
@@ -21,7 +19,11 @@ function getCityName(lat, long) {
 
     fetch(geoLocationApi, { mode: 'cors' })
         .then(response => {
-            return response.json()
+            if (response.status == 200) {
+                return response.json()
+            } else {
+                console.log(response.status)
+            }
            
         })
         .then(data => {
@@ -30,11 +32,47 @@ function getCityName(lat, long) {
                 
                 let cityComponent = addressComponents.find(component => 
                     component.types.includes('locality'))
-                
-                cityComponent? console.log(cityComponent.long_name): console.log('no city found')
-
+                if (cityComponent) {
+                    const cityName = cityComponent.long_name
+                    console.log(cityName)
+                        
+                } else {
+                    console.log('no city found')
+                }
             }
             
         })
-  
+        .catch((err) => {
+            console.log('Error fetching data: ' + err)
+        })
 }
+
+//Current temperature(cast type),feels like, wind, humidity
+async function getWeatherData() {
+    const weatherAPI = "https://weather.visualcrossing.com/VisualCrossingWebServices/rest/services/timeline/ofallon?key=5RLUDDTYJ228UMKJ3WH4NJEZK"
+
+    try {
+        const weatherResponse = await fetch(weatherAPI, { mode: 'cors' })
+        const weatherData = await weatherResponse.json()
+        const weatherToday = weatherData.days[0]
+        console.log(weatherData)
+
+        //location values
+        const address = weatherData.resolvedAddress
+        console.log(address)
+
+        // Weather values
+        const currentTemperature = weatherToday.temp
+        const maxTemperature = weatherToday.tempmax
+        const minTemperature = weatherToday.tempmin
+        const humilityLevel = weatherToday.humility
+        const windSpeed = weatherToday.windSpeed
+        const precipitation = weatherToday.precip
+        console.log(currentTemperature,precipitation)
+
+    } catch(error) {
+        console.error('Error fetching weather data ', error )
+    }
+}
+
+getWeatherData()
